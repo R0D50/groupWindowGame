9fe9a6a89b8d529a016d142661b80837fd6d9148
boysuperman25 <markmcglothen9606@gmail.com>
2025-03-31 09:22:11 -0500

    Add files via upload

---
+++ #WINDOW GAME#.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+WIDTH = 250
+HEIGHT = 100
+
+window = tk.Tk()
+window.title("Wooden Block")
+window.geometry(f"{WIDTH}x{HEIGHT}")
+
+image_path = "C:\\Users\\markm\\OneDrive\\Desktop\\Screenshot 2025-03-31 091325.png"
+image = Image.open(image_path)
+photo_image = ImageTk.PhotoImage(image)
+
+image_label = tk.Label(window, image=photo_image)
+image_label.pack()
+
+window.mainloop()
